8ae52961928cef04033d653687121b3189d284bb
Kevin Smith <falchard@live.com>
2023-05-17 23:08:23 -0700

    Finished Logic and Comments

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,8 @@ const spec = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Asks user for length of password
 function getLength(){
   var length = Number(prompt("Please enter password length:", "8-128"));
   if(length < 8 || length > 128 || isNaN(length)){
@@ -14,10 +16,9 @@ function getLength(){
   }
   return length;
 }
-
+//Asks user if they want to use Upper case letters.
 function getUpperCase(){
-  var upperCase = prompt("Do you want an upper case letter?", "Y/N").toUpperCase();
-  switch(upperCase){
+  switch(prompt("Do you want an upper case letter?", "Y/N").toUpperCase()){
     case "Y":
       return true;
     case "N":
@@ -26,9 +27,9 @@ function getUpperCase(){
       return getUpperCase();
   }
 }
+//Asks user if they want to use Lower case letters.
 function getLowerCase(){
-  var lowerCase = prompt("Do you want an lower case letter?", "Y/N").toUpperCase();
-    switch(lowerCase){
+    switch(prompt("Do you want an lower case letter?", "Y/N").toUpperCase()){
     case "Y":
       return true;
     case "N":
@@ -37,9 +38,9 @@ function getLowerCase(){
       return getLowerCase();
   }
 }
+//Asks user if they want to use numerical digits.
 function getNumeric(){
-  var numeric = prompt("Do you want numerical digits?", "Y/N").toUpperCase();
-    switch(numeric){
+    switch(prompt("Do you want numerical digits?", "Y/N").toUpperCase()){
     case "Y":
       return true;
     case "N":
@@ -48,9 +49,9 @@ function getNumeric(){
       return getNumeric();
   }
 }
+//Asks user if they want to use special characters.
 function getSpecial(){
-  var special = prompt("Do you want special characters?", "Y/N").toUpperCase();
-    switch(special){
+    switch(prompt("Do you want special characters?", "Y/N").toUpperCase()){
     case "Y":
       return true;
     case "N":
@@ -60,68 +61,107 @@ function getSpecial(){
   }
 }
 
-function generatePassword(){
-  var password = "";
-  var length = getLength();
-  var upperCase = false;
-  var lowerCase = false;
-  while(upperCase == false && lowerCase == false){
-    upperCase = getUpperCase();
-    lowerCase = getLowerCase();
+// Randomly determines how many of each character type to use.
+//   length: The length of the password.
+//   charCase: an array of 4 booleans.
+function determineTypeCount(length, charCase){
+  var count = [0, 0, 0, 0];
+  if(charCase[0]){
+      count[0]++;
+      length--;
   }
-  var numeric = getNumeric();
-  var special = getSpecial();
-  var upperCount = 0;
-  var lowerCount = 0;
-  var numericCount = 0;
-  var specialCount = 0;
-  if(upperCase){
-    upperCount++;
+  if(charCase[1]){
+    count[1]++;
     length--;
   }
-  if(lowerCase){
-    lowerCount++;
+  if(charCase[2]){
+    count[2]++;
     length--;
   }
-  if(numeric){
-    numericCount++;
+  if(charCase[3]){
+    count[3]++;
     length--;
   }
-  if(special){
-    specialCount++;
-    length--;
-  }
+
   while(length > 0){
-    var charType = Math.floor(Math.random() * 4 + 1);
-    switch(charType){
+    switch(Math.floor(Math.random() * 4)){
+      case 0:
+        if(charCase[0]){
+          count[0]++;
+          length--;
+        }
+        break;
       case 1:
-        if(upperCase){
-          upperCount++;
+        if(charCase[1]){
+          count[1]++;
           length--;
         }
         break;
       case 2:
-        if(lowerCase){
-          lowerCount++;
+        if(charCase[2]){
+          count[2]++;
           length--;
         }
         break;
       case 3:
-        if(numeric){
-          numericCount++;
-          length--;
-        }
-        break;
-      case 4:
-        if(special){
-          specialCount++;
+        if(charCase[3]){
+          count[3]++;
           length--;
         }
         break;
     }
   }
-  password = String(upperCount) + String(lowerCount) + String(numericCount) + String(specialCount)
+  return count;
+}
+
+// Randomly picks characters based on the parameters given.
+//   length: The length of the password.
+//   typeCount: An array of whole numbers of how many for each character type.
+function randomCharacters(length, typeCount){
+  var password = "";
+  while(length > 0){
+    switch(Math.floor(Math.random() * 4)){
+      case 0:
+        if(typeCount[0] > 0){
+          password += char[Math.floor(Math.random() * char.length)].toUpperCase();
+          typeCount[0]--;
+          length--;
+        }
+      case 1:
+        if(typeCount[1] > 0){
+          password += char[Math.floor(Math.random() * char.length)];
+          typeCount[1]--;
+          length--;
+        }
+      case 2:
+        if(typeCount[2] > 0){
+          password += num[Math.floor(Math.random() * num.length)];
+          typeCount[2]--;
+          length--;
+        }
+      case 3:
+        if(typeCount[3] > 0){
+          password += spec[Math.floor(Math.random() * spec.length)];
+          typeCount[3]--;
+          length--;
+        }
+    }
+  }
   return password;
+}
+
+//Generates a random password based on input given by the user.
+function generatePassword(){
+  var length = getLength();
+  var charCase = [false, false, false, false];
+  while(charCase[0] == false && charCase[1] == false){
+    charCase[0] = getUpperCase();
+    charCase[1] = getLowerCase();
+  }
+  charCase[2] = getNumeric();
+  charCase[3] = getSpecial();
+
+  return randomCharacters(length, determineTypeCount(length, charCase));
 }
 
 // Write password to the #password input
